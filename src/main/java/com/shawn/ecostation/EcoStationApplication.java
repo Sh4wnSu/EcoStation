2ed0dbda68448e075a03b9af94db231c28a026bc
shawn <lanter_shawn666@sjtu.edu.cn>
2022-07-31 13:53:34 +0800

    First version

---
+++ src/main/java/com/shawn/ecostation/EcoStationApplication.java
@@ -0,0 +1,13 @@
+package com.shawn.ecostation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shawn/ecostation/EcoStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawn.ecostation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
